f8291522e6bc5486eb16ec79b3d088433f660162
Megha <meghacute786@gmail.com>
2024-01-24 23:35:54 +0530

    TO DO List Using jQuery

---
+++ jQuery.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  // Your jQuery code here
+  console.log('hey i am jQuery');
+  $('#inp').change(function () {
+    var input = $(this).val();
+    $('ul').append(
+      '<li>' +
+        input +
+        '<i class="fas fa-check right"></i> <i class="fa-solid fa-trash"></i> </li>'
+    );
+    $(this).val('');
+  });
+  $('ul').on('click', '.fa-trash', function () {
+    $(this).parent('li').fadeOut(500);
+  });
+  // $('ul').on('click', '.fa-check', function () {
+  //   $(this).parent('li').toggleClass('checked');
+  // });
+  $('ul').click(function () {
+    $(this).children('li').toggleClass('checked');
+  });
+});
